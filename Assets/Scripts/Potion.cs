ea6ca5e20d573d17edda3ade7f9b224be80bfdb6
padaPoon <padawankerdphol@gmail.com>
2025-11-25 18:34:13 +0700

    enumPotionSize

--- Assets/Scripts/Potion.cs
+++ Assets/Scripts/Potion.cs
@@ -1,8 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
+public enum PotionSize
+{ 
+    Small,
+    Big
+}
 
 // ไอเท็มเพิ่มพลังชีวิต
 public class Hearth : Item
 {
+    //set potion item value
+    public PotionSize potionSize;
+
+    public void Init(PotionSize size)
+    {
+        potionSize = size;
+
+        switch (size)
+        {
+            case PotionSize.Small: 
+                itemValue = 10; //heal 10 point
+                break;
+
+            case PotionSize .Big:
+                itemValue = 25; //heal 25 point
+                break;
+        }
+    }
+
+
     public override void Use(CharacterMovement player)
     {
         if (player)
