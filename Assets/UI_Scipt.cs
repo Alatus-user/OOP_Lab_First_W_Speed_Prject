9b1a8ea1f05273072c203dfd77f3c3185b5fda60
padaPoon <padawankerdphol@gmail.com>
2025-11-28 15:03:55 +0700

    ChangerSence

--- Assets/UI_Scipt.cs
+++ Assets/UI_Scipt.cs
@@ -28,7 +28,7 @@ public class UI_Scipt : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
 
